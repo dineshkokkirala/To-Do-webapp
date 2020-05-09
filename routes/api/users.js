f34266faa2f76e57bbbf682026a11c479e84dc5e
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-05-09 21:05:30 +0530

    Registration and Login Done

---
+++ routes/api/users.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+//  api/users/
+//  public
+//  Register a user
+
+router.post(
+  "/",
+  [
+    check("name", "Please add Name").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //res.send("Success");
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      user = new User({ name, email, password });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      //const users =
+      await user.save();
+
+      //res.json(users);
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const connectDB = require("./config/db");
 //app.get("/", (req, res) => res.send("Hello from Dinesh!"));
 connectDB();
 
+//init middleware
+app.use(express.json({ extended: false }));
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on ${port}`));
