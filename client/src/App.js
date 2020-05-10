b80c3eef09e5f43e032c9fb3c65e9e4f36833b4a
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-05-10 12:45:59 +0530

    Client side Initial setup and Navbar Done

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  );
+};
+
+export default App;
